3cf089b487550dadae8c3c81935c6dee17b0cc5c
mariiaws <wooglay@gmail.com>
2021-12-05 16:51:14 +0300

    Create file main.js, slider-swiper and added plugin swiper-bundle.min.css, swiper-bundle.min.js.

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.slide-button--next',
+        prevEl: '.slide-button--prev',
+    },
+
+
+});
