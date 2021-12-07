2b45f65adc4229f65a902bd235407e809bc13fa6
mariiaws <wooglay@gmail.com>
2021-12-07 21:06:26 +0300

    Fixed: style _reviews.scss and reviews html block

--- js/main.js
+++ js/main.js
@@ -5,25 +5,13 @@ const swiper = new Swiper('.swiper', {
 
     // Navigation arrows
     navigation: {
-        nextEl: '.slide-button--next',
-        prevEl: '.slide-button--prev',
+        nextEl: '.slide-button--next, .reviews-slide-button--next',
+        prevEl: '.slide-button--prev, .reviews-slide-button--prev',
+
     },
 
 
 });
 
 
-/*
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.slide-button--next',
-        prevEl: '.slide-button--prev',
-    },
-
-
-});*/
