baba11fe01a2eeac6fa68beaac9e829800a7e965
mariiaws <wooglay@gmail.com>
2021-12-25 20:26:17 +0300

    Create: block in index.html, also-watching and _also-watching.scss completely. Added one sliders.
    Delete: in _header.scss, _hotel.scss  all comments and changing properties. main.js added object for sliders.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,52 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
+const swiper = new Swiper('.hotel-grid__swiper', {
+// Optional parameters
     direction: 'horizontal',
     loop: true,
 
     // Navigation arrows
     navigation: {
-        nextEl: '.slide-button--next, .button-slide-next',
-        prevEl: '.slide-button--prev, .button-slide-prev',
+        nextEl: '.slide-button--next',
+        prevEl: '.slide-button--prev',
 
     },
 
+});
+
+
+
+new Swiper('.reviews-swiper', {
+// Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.button-slide-next',
+        prevEl: '.button-slide-prev',
+
+    },
 
 });
 
+
+new Swiper('.watching-swiper', {
+
+ /*   //Automatic scrolling
+    speed: 2000,
+    autoplay: true,*/
+
+
+// Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.button-slide-next',
+        prevEl: '.button-slide-prev',
+
+    },
+
+});
 
 
